749c77cf096418c617111919835cf526c5b7d674
Santi Mejia Ocampo <santimejiaok@gmail.com>
2023-07-14 22:09:52 -0500

    things

--- servidor/routes/pay.js
+++ servidor/routes/pay.js
@@ -1,11 +1,11 @@
 import Stripe from "stripe";//portal de pagos para conectarse a su api
-const striper = Stripe('sk_test_51NJ7RVIppCBXGZn9L5S9Tko1n9zO4SnMgpq0RGokS4VWu6kxcpgly5VhGqNDPrckYDEAQFyAdvhJ4VbyEJte8Y4P003AbHuC5x');//clave de acceso stripe para recibir el pago
+const stripe = Stripe('sk_test_51NJ7RVIppCBXGZn9L5S9Tko1n9zO4SnMgpq0RGokS4VWu6kxcpgly5VhGqNDPrckYDEAQFyAdvhJ4VbyEJte8Y4P003AbHuC5x');//clave de acceso stripe para recibir el pago
 
 //funcion que permite conectarse a la api y realizar los pagos, recibe una promesa
 export const pay = async (req,res) => {
     let {amount, id} = req.body
     try {
-        const payment = await striper.paymentIntents.create({
+        const payment = await stripe.paymentIntents.create({
             amount,
             currency: 'COP',
             description: 'shop',
